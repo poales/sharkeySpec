be273457e412e9066d9f7a76a2b268d395626072
poales <38336601+poales@users.noreply.github.com>
2019-06-05 11:12:47 -0400

    Update ss_readType.R

--- R/ss_readType.R
+++ R/ss_readType.R
@@ -6,8 +6,8 @@
 
 ss_readType <- function(location,pattern){
   file.names <- as.list(dir(location,pattern=pattern))
-  file.names <- lapply(file.names,function(x) paste0(location,"/",x))
-  data <- lapply(file.names, ss_read)
+  file.names2 <- lapply(file.names,function(x) paste0(location,"/",x))
+  data <- lapply(file.names2, ss_read)
   file.names <- trimws(sub(pattern="(.*?)\\.dat(.*)000.*$",replacement = "\\1 \\2",x=file.names))
   names(data) <- file.names
   data <- tibble::set_tidy_names(data)
