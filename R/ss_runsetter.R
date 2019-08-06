617f5e78cceab9333f35c2c57200366948a8ad3a
poales <38336601+poales@users.noreply.github.com>
2019-08-06 16:18:38 -0400

    Update ss_runsetter.R

--- R/ss_runsetter.R
+++ R/ss_runsetter.R
@@ -5,7 +5,7 @@
 #' @param df The data table (singular) to analyze
 #' @param threshhold The difference in the chosen column that is to be considered a new measurement.
 #' @param aslist When FALSE, returns a dataframe with an ID column that distinguishes separate measurements. When TRUE, returns the same but divided into a list, with one element per measurement.
-#' @param column The chosen column to study for differences in threshhold. Defaults to "Time," used by me to differentiate different measurements taken in the same folder but separated by time.
+#' @param column The chosen column to study for differences in threshhold. Defaults to "Time," used by me to differentiate different measurements taken in the same file but separated by time.
 #' @name ss_runsetter
 #' @export
 
@@ -14,15 +14,6 @@
 ss_runsetter<- function(df,threshhold=2,aslist = FALSE,column = "Time"){
   #scan through the dataframe and look for gaps
   #set gaps to be the break in runs with an ID column
-  #first test for a time column - if none exist, send the df thru bookkeeping
-  if(!(column %in% colnames(df))){
-    cat("Error setting runs: Chosen column name not in df")
-    return("")
-  }
-  if("ID" %in% colnames(df)){
-    cat("Error setting runs: ID column already extant in df")
-    return("")
-  }
   TDIFF <- function(df){
     diff <- df[-1,column]-df[-nrow(df),column]
     changes <- which(diff>threshhold | diff<(-1*threshhold))
@@ -33,16 +24,29 @@ ss_runsetter<- function(df,threshhold=2,aslist = FALSE,column = "Time"){
     }
     return(df)
   }
-  df <- tibble::add_column(df,ID=1)
-  df <- TDIFF(df)
-  if(aslist){
-    mylist <- list()
-    for(i in unique(df$ID)){
-      mylist[[i]] <- dplyr::filter(df,df$ID==i)
+
+  if((column %in% colnames(df))){
+    if(!("ID" %in% colnames(df))){
+
+      df <- tibble::add_column(df,ID=1)
+      df <- TDIFF(df)
+      if(aslist){
+        mylist <- list()
+        for(i in unique(df$ID)){
+          mylist[[i]] <- dplyr::filter(df,df$ID==i)
+        }
+        df <- mylist
+        #df <- lapply(df,function(x) select(x,-ID))
+      }
+
+    } else {
+      cat("Error setting runs: ID column already extant in df")
     }
-    df <- mylist
-    #df <- lapply(df,function(x) select(x,-ID))
+
+  } else{
+    cat("Error setting runs: Chosen column name not in df")
   }
+
   return(df)
 
 }
