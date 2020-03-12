c32e891970c032fece5cec74bfeecb322d166040
Alan McClain <mcclai54@msu.edu>
2020-03-12 14:27:04 -0400

    Update ss_read.R
    Fixed broken reading of >4 column files.

--- R/ss_read.R
+++ R/ss_read.R
@@ -8,10 +8,22 @@
 ss_read <- function(location){
   #require(magrittr)
   #require(tidyverse)
-  suppressMessages(dat <- tryCatch({
-    readr::read_delim(location,delim="\t",col_names = c("Time","Raw_Voltage","Ref","DeltaA"))
-  },error=function(e){
-    readr::read_delim(location,delim = "\t",col_names = F)
-  }))
+  count <- round(mean(readr::count_fields(location,tokenizer = readr::tokenizer_tsv())))
+  labs <- F
+  if(count %% 4 == 0){
+    count <- count %/% 4
+    labs <- c("Time","Raw_Voltage","Ref","DeltaA")
+    if(count>1){
+      lb <- labs
+      for(i in 2:count){
+        lb <- c(lb,paste0(labs,"_LED",i))
+      }
+      labs <- lb
+    }
+  }
+  suppressMessages(dat <- readr::read_delim(location,delim="\t",col_names = labs))
+  
+  
+  
   return(dplyr::mutate_all(dat,as.numeric))
 }
